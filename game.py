9a579b57ed0e0629d3b964f626454e080b1e9104
joanavrsilva <joanavrsilva@gmail.com>
2022-03-15 13:32:16 +0000

    Add index, progress levels and berries feauture

--- game.py
+++ game.py
@@ -63,6 +63,52 @@ pygame.display.flip()
 
 
 # game
+class game:
+    def __init__(self, level, score):
+        self.paused = True
+        self.ghostsDelays = 1
+        self.ghostsCount = 0
+        self.pacmanDelays = 1
+        self.pacmanCount = 0
+        self.timerDelay = 10
+        self.timerCount = 0
+        self.ghostsAttack = False
+        self.highScore = self.getHighScore()
+        self.score = score
+        self.level = level
+        self.lives = 3
+        self.ghosts = [Ghost(15.0, 14.5, "red", 0), Ghost(18.0, 12.5, "blue", 1), Ghost(18.0, 14.5, "pink", 2), Ghost(18.0, 16.5, "orange", 3)]
+        self.pacman = Pacman(27.0, 14.5) # pacman starts in the of the board
+        self.total = self.getCount()
+        self.ghostScore = 150
+        self.levels = [[300, 200], [100, 400], [100, 400], [0, 500]]
+        random.shuffle(self.levels)
+
+
+        # index and progress levels
+        self.ghostStatus = [[1, 0], [0, 0], [1, 0], [0, 0]]
+        index = 0
+        for status in self.ghostStatus:
+            status[0] = randrange(2)
+            status[1] = randrange(self.levels[index][status[0]] + 1)
+            index += 1
+        self.collect = 0
+        self.start = False
+        self.gameOver = False
+        self.gameOverRestartScore = 0
+        self.points = []
+        self.pointsTimer = 10
+
+        # berries
+        self.berryStatus = [150, 350, False]
+        self.berryLocation = [21.0, 14.5]
+        self.berries = ["tile080.png", "tile081.png", "tile082.png", "tile083.png", "tile084.png", "tile085.png", "tile086.png", "tile087.png"]
+        self.berriesCollected = []
+        self.levelTimer = 0
+        self.berryScore = 100
+        self.lockTimer = 100
+        self.lockIn = True
+        self.extraLife = False
 
 # pacman
 
