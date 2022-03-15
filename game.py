58f366718e972d9b1174395bf191adfaa749a63b
joanavrsilva <joanavrsilva@gmail.com>
2022-03-15 13:06:19 +0000

    Initial structure

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame
+import math
+from random import randrange
+import random
+import copy
+import os
+
+# paths
+board = ".../assets/board/"
+text = ".../assets/text/"
+data = ".../assets/data/"
+element = ".../assets/elements/"
+
+# board
+
+# game
+
+# pacman
+
+# ghosts
+
+# game over
+
+# display lives
+
+# new level
+
+# score
+
